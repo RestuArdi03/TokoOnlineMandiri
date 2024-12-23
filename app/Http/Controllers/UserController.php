c1c12d5c93fa80b21facd93154d17754efac8f41
“RestuArdi03” <restuardi03@gmail.com>
2024-12-23 15:14:57 +0700

    Membuat ImageHelper

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use App\Helpers\ImageHelper;
 
 class UserController extends Controller
 {
@@ -37,6 +39,42 @@ class UserController extends Controller
     public function store(Request $request)
     {
         //
+        $validatedData = $request->validate([
+            'nama' => 'required|max:255',
+            'email' => 'required|max:255|email|unique:user',
+            'role' => 'required',
+            'hp' => 'required|min:10|max:13',
+            'password' => 'required|min:4|confirmed',
+            'foto' => 'image|mimes:jpeg,jpg,png,gif|file|max:1024',
+        ], $messages = [
+            'foto.image' => 'Format gambar gunakan file dengan ekstensi jpeg, jpg, png, atau gif.',
+            'foto.max' => 'Ukuran file gambar Maksimal adalah 1024 KB.'
+        ]);
+        $validatedData['status'] = 0;
+
+        // menggunakan ImageHelper
+        if ($request->file('foto')) {
+            $file = $request->file('foto');
+            $extension = $file->getClientOriginalExtension();
+            $originalFileName = date('YmdHis') . '_' . uniqid() . '.' . $extension;
+            $directory = 'storage/img-user/';
+            // Simpan gambar dengan ukuran yang ditentukan
+            ImageHelper::uploadAndResize($file, $directory, $originalFileName, 385, 400); // null (jika tinggi otomatis)
+            // Simpan nama file asli di database
+            $validatedData['foto'] = $originalFileName;
+        }
+
+        // password kombinasi
+        $password = $request->input('password');
+        $pattern = '/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).+$/';
+        // huruf kecil ([a-z]), huruf besar ([A-Z]), dan angka (\d) (?=.*[\W_]) simbol karakter (non-alphanumeric)
+        if (preg_match($pattern, $password)) {
+            $validatedData['password'] = Hash::make($validatedData['password']);
+            User::create($validatedData, $messages);
+            return redirect()->route('backend.user.index')->with('success', 'Data berhasil tersimpan');
+        } else {
+            return redirect()->back()->withErrors(['password' => 'Password harus terdiri dari kombinasi huruf besar, huruf kecil, angka, dan simbol karakter.']);
+        }
     }
 
     /**
